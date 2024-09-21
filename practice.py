b6a1f96c56dd2dbbacbe09cfa1dc867dc994e76f
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2024-09-21 18:13:02 +0530

    operators practice

---
+++ practice.py
@@ -0,0 +1,32 @@
+x = 5
+y, z = 4, 8
+
+
+print(x, y, z)
+
+
+name = "mallikharjuna reddy"
+
+print(name)
+# comparison operators
+print(x>y)
+print(x<y)
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
+
+# arithmetic operators
+print(x*y)
+print(x+y)
+print(x-y)
+print(x/y)
+print(x//y)
+print(x**y)
+
+a = True
+b = False
+
+print(a and b)
+print(a or b)
+print(not a)
