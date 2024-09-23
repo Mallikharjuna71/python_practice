0b7d0fd34cb477e657ee1c695f6abfd7e4374a9a
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2024-09-23 18:06:11 +0530

    python practice

--- dict.py
+++ dict.py
@@ -1,1 +1,25 @@
 d = {}
+
+d['name'] = 'Madara uchiha'
+d['kills'] = 80000
+d['nick_name'] = 'ghost of the uchiha'
+
+l = ['pet', 'age', 'skills']
+d1 = dict.fromkeys(l)
+
+k = d1.pop('skills')
+l = d1.popitem()
+print(d1)
+
+d.update(d1)
+print(d, k, l)
+
+d['pet'] = 'kurama'
+d['skills'] = 'susano'
+d['eyes'] = ['EMS', 'Rinnegan']
+
+print(d)
+del d['pet']
+print(d)
+print(len(d))
+
---
+++ logging.py
@@ -0,0 +1,8 @@
+import datetime
+date = datetime.date(2019, 11, 27)
+print(date)
+print(datetime.date.today())
+
+print(datetime.time(23, 45, 59))
+print(datetime.datetime.now())
+
